a03398515ba7a235db635fbcbe242c9ecc1e77b6
Grace Gao <grace5829@gmail.com>
2024-03-04 15:36:25 -0500

    updating db information

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from enum import Enum
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://gracegao:9455@localhost/TravelHub'
@@ -11,25 +11,54 @@ db=SQLAlchemy(app)
 CORS(app)
 
 # to load the db 
-# new terminal, cd to correct folder, type python and enter, from app iport db,app ,
+# new terminal, cd to correct folder, type python and enter, run "from app import db,app" ,
 # app.app_context().push(), db.create_all()
 
-class Event(db.Model):
+class RSVPEnum(Enum):
+    YES = 'yes'
+    NO = 'no'
+    MAYBE = 'maybe'
+    PENDING = 'pending'
+
+class GenderEnum(Enum):
+    MALE = 'male'
+    FEMALE = "female"
+
+class Guests(db.Model):
+    __tablename__ = 'guests'
     id=db.Column(db.Integer, primary_key=True)
-    description=db.Column(db.String(100), nullable=False)
+    notes=db.Column(db.String(100), nullable=False)
+    firstName=db.Column(db.String(20), nullable=False)
+    lastName=db.Column(db.String(20), nullable=False)
+    gender=db.Column(db.Enum(GenderEnum), nullable=False)
+    age=db.Column(db.Integer, nullable=False)
+    amountDue=db.Column(db.Integer, nullable=False)
+    RSVP = db.Column(db.Enum(RSVPEnum), nullable=False)
     created_at=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"Event: {self.description}"
+        return f"<Guest(id={self.id}, notes={self.notes}, RSVP={self.RSVP}, gender={self.gender}, created_at={self.created_at})>"
 
-    def __init__(self, description):
-        self.description=description
+    def __init__(self, notes, firstName, lastName, gender, age, amountDue, RSVP):
+        self.notes = notes
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.age = age
+        self.amountDue = amountDue
+        self.RSVP = RSVP
 
-def format_event(event):
+def format_event(guests):
     return {
-        "description":event.description,
-       'id': event.id,
-        'created_at': event.created_at
+        "notes":guests.notes,
+        'id': guests.id,
+        'created_at': guests.created_at,
+        'firstName': guests.firstName,
+        'lastName': guests.lastName, 
+        'gender': guests.gender,
+        'age': guests.age,
+        'amountDue': guests.amountDue,
+        'RSVP': guests.RSVP
     }
 
 
@@ -39,47 +68,68 @@ def home():
 
 
 # create an event 
-@app.route('/events', methods=['POST'])
+@app.route('/guests', methods=['POST'])
 def create_event():
-    description=request.json['description']
-    event= Event(description)
-    db.session.add(event)
+    data = request.json
+    app.logger.info(f"Received payload: {data}")
+
+    # Extracting data from the request
+    notes = data.get('notes')
+    firstName = data.get('firstName')
+    lastName = data.get('lastName')
+    gender = data.get('gender')
+    age = data.get('age')
+    amountDue = data.get('amountDue')
+    RSVP = data.get('RSVP')
+
+    # Creating a new guest instance
+    new_guest = Guests(
+        notes=notes,
+        firstName=firstName,
+        lastName=lastName,
+        gender=gender,
+        age=age,
+        amountDue=amountDue,
+        RSVP=RSVP
+    )
+
+    db.session.add(new_guest)
     db.session.commit()
     return format_event(event)
 
 # get all events, dont need 'GET'; set by default
-@app.route('/events', methods=['GET'])
-def get_events():
-    events=Event.query.order_by(Event.id.asc()).all()
-    event_list=[]
-    for event in events:
-        event_list.append(format_event(event))
-    return {'events':event_list}
+# @app.route('/events', methods=['GET'])
+# def get_events():
+#     events=Event.query.order_by(Event.id.asc()).all()
+#     event_list=[]
+#     for event in events:
+#         event_list.append(format_event(event))
+#     return {'events':event_list}
 
 # get single event
-@app.route('/event/<id>', methods=['GET'])
-def get_event(id):
-    event=Event.query.filter_by(id=id).one()
-    formatted_event=format_event(event)
-    return {'event':formatted_event}
+# @app.route('/event/<id>', methods=['GET'])
+# def get_event(id):
+#     event=Event.query.filter_by(id=id).one()
+#     formatted_event=format_event(event)
+#     return {'event':formatted_event}
 
 # delete event 
-@app.route('/event/<id>', methods=['DELETE'])
-def delete_event(id):
-    event=Event.query.filter_by(id=id).one()
-    db.session.delete(event)
-    db.session.commit()
-    return f'Event (id:{id}) deleted'
+# @app.route('/event/<id>', methods=['DELETE'])
+# def delete_event(id):
+#     event=Event.query.filter_by(id=id).one()
+#     db.session.delete(event)
+#     db.session.commit()
+#     return f'Event (id:{id}) deleted'
 
 
 # update event 
-@app.route('/events/<id>', methods=['PUT'])
-def update_event(id):
-    event=Event.query.filter_by(id=id)
-    description=request.json['description']
-    event.update(dict(description=description, created_at=datetime.utcnow()))
-    db.session.commit()
-    return {'event':format_event(event.one())}
+# @app.route('/events/<id>', methods=['PUT'])
+# def update_event(id):
+#     event=Event.query.filter_by(id=id)
+#     description=request.json['description']
+#     event.update(dict(description=description, created_at=datetime.utcnow()))
+#     db.session.commit()
+#     return {'event':format_event(event.one())}
 
 
 
