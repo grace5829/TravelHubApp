313761b0435f5c94e091cd3a666fddfffc8f567d
Grace Gao <grace5829@gmail.com>
2024-04-03 16:01:12 -0400

    adding form to add Events

--- travelhub-frontend/pages/event/allEvents.tsx
+++ travelhub-frontend/pages/event/allEvents.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Link from "next/link";
 import { GuestsContext } from "../_app";
+import EventForm from "./eventForm";
+import { useRouter } from "next/router";
 
 const TableWrapper = styled.div`
   margin: 10px;
@@ -9,10 +11,10 @@ const TableWrapper = styled.div`
   flex-wrap: wrap;
   justify-content: center;
 `;
-const EachEvent =  styled(Link)`
+const EachEvent = styled(Link)`
   box-shadow: 5px 5px 5px gray;
   border-radius: 5px;
-  color:black;
+  color: black;
   text-decoration: none;
   margin: 8px;
   padding: 6px;
@@ -29,27 +31,66 @@ const EachEventInfo = styled.span`
 `;
 
 export default function AllEvents() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getDate();
+  const startDate = new Date(year, month, day);
+  const endDate = new Date(year, month, day);
+
   const { events, setEvents } = useContext(GuestsContext);
+  const [eventForm, seteventForm] = useState(false);
+  const [method, setMethod] = useState("POST");
+  const [currentEvent, setCurrentEvent] = useState({
+    name: "",
+    location: "",
+    start_date: "",
+    end_date: "",
+    notes: "",
+  });
+
+  const addEvent = () =>{
+    setMethod("POST")
+    seteventForm(!eventForm)
+  }
 
   return (
     <div>
+      <div>
+
+         {eventForm?
+      <EventForm 
+      setHidden={seteventForm}
+      hidden={eventForm}
+      currentEvent={currentEvent}
+      setCurrentEvent={setCurrentEvent}
+      method={method}
+      /> : 
+      <button onClick={() => addEvent() }> Add Event</button>
+    }
+    </div>
       {events ? (
-        <TableWrapper >
+        <TableWrapper>
           {events.map((event) => (
-            <EachEvent key={event.location + event.name} href={{pathname:`/${event.name.toLowerCase()}`, query:{ id:event.id}}} >
-              <EachEventInfo><h3>{event.name} </h3></EachEventInfo>
+            <EachEvent
+              key={event.location + event.name}
+              href={{
+                pathname: `/${event.name.toLowerCase()}`,
+                query: { id: event.id },
+              }}
+            >
               <EachEventInfo>
-                Start:{event.start_date.slice(0, 16)} 
+                <h3>{event.name} </h3>
               </EachEventInfo>
-              <EachEventInfo>
-               End:{event.end_date.slice(0, 16)}
-              </EachEventInfo>
+              <EachEventInfo>Start:{`${event.start_date.slice(0, 16)}`}</EachEventInfo>
+              <EachEventInfo>End:{`${event.end_date.slice(0, 16)}`}</EachEventInfo>
               <EachEventInfo>Location:{event.location} </EachEventInfo>
               <EachEventInfo>Notes:{event.notes} </EachEventInfo>
             </EachEvent>
-          ))}{" "}
+          ))}
         </TableWrapper>
       ) : null}
+   
     </div>
   );
 }
---
+++ travelhub-frontend/pages/event/eventForm.tsx
@@ -0,0 +1,169 @@
+import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
+import styled from "styled-components";
+import React from "react";
+import { Guest, Event, GuestsContext } from "../_app";
+
+const SidePanels = styled.span`
+  z-index: 1;
+  width: 30vw;
+  padding: 10px;
+  background: gray;
+  height: 100vh;
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
+
+const FormWrapper = styled.form`
+  display: grid;
+`;
+
+export default function EventForm({
+  setHidden,
+  hidden,
+  currentEvent,
+  setCurrentEvent,
+  method,
+}: {
+  setHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  hidden: boolean;
+  currentEvent: Event;
+  setCurrentEvent: React.Dispatch<React.SetStateAction<Event>>;
+  method: string;
+}) {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getUTCDate();
+  const startDates = new Date(year, month, day);
+  const endDate = new Date(year, month, day);
+  const [startDate, setStartDate] = useState('');
+
+  const { setEvents, events } = useContext(GuestsContext);
+  const defaultEvent: Event = {
+    name: "",
+    location: "",
+    start_date: "",
+    end_date: "",
+    notes: "",
+  };
+  function formatDate(date: Date): string {
+    date = new Date(date);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Ensure two digits
+    const day = date.getUTCDate().toString().padStart(2, "0"); // Ensure two digits
+    return `${year}-${month}-${day}`;
+  }
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    const name = event.target.name;
+    const value = event.target.value;
+  
+      setCurrentEvent((values) => ({ ...values, [name]: value }));
+    
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!currentEvent.name.trim() || !currentEvent.location.trim()) {
+      let errorMessage = "";
+      if (!currentEvent.name.trim()) {
+        errorMessage += "Please enter an event name.\n";
+      }
+      if (!currentEvent.location.trim()) {
+        errorMessage += "Please enter a location.\n";
+      }
+      alert(errorMessage);
+      return;
+    }
+    console.log(currentEvent);
+    let API;
+    if (method === "POST") {
+      API = `http://127.0.0.1:5000/events`;
+    } else {
+      API = `http://127.0.0.1:5000/events/${currentEvent.id}`;
+    }
+    try {
+      const response = await fetch(API, {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(currentEvent),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to update. Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setEvents(result.events);
+    } catch (error: any) {
+      console.error("Error fetching data:", error);
+    }
+
+    if (method === "POST") {
+      setCurrentEvent(defaultEvent);
+    }
+    setHidden(!hidden);
+  };
+
+  const hideSideBar = () => {
+    setHidden(!hidden);
+    setCurrentEvent(defaultEvent);
+  };
+  
+  return (
+    <SidePanels>
+      <button onClick={() => hideSideBar()}>X</button>
+      <FormWrapper onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={currentEvent.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="location">
+          Location:
+          <input
+            type="text"
+            name="location"
+            value={currentEvent.location}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="start_date">
+          Start date:
+          <input
+            type="date"
+            name="start_date"
+            // value={formatDate(currentEvent.start_date)}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="end_date">
+          End date:
+          <input
+            type="date"
+            name="end_date"
+            // value={formatDate(currentEvent.end_date)}
+            onChange={handleChange}
+          />
+        </label>
+        {/* <label>
+        Start Date:
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+      </label> */}
+        <input type="submit" />
+      </FormWrapper>
+    </SidePanels>
+  );
+}
