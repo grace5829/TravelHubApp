8cb81470ef5986b4d9405717a24495d52a5c7cd5
Grace Gao <grace5829@gmail.com>
2024-04-13 13:00:22 -0400

    creating new tabs page for guest and expenses

---
+++ travelhub-frontend/pages/components/tabs.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Tabs({ config }: any) {
+const [activeTab, setActiveTab]=useState(0)
+
+
+  return (
+    <div>
+{config.map((entry:any, index:any)=>(
+    <div onClick={()=>setActiveTab(index)}>
+        {entry.header}
+         </div>
+))
+
+}      
+<div>
+    {config[activeTab].component}
+</div>
+    </div>
+  );
+}
