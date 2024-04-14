a14a04ca5c998744da26c1e125292629a4de0c48
Grace Gao <grace5829@gmail.com>
2024-04-14 12:31:32 -0400

    moving allguest to a new file

--- travelhub-frontend/pages/components/allGuests.tsx
+++ travelhub-frontend/pages/components/allGuests.tsx
@@ -1,11 +1,148 @@
-import { useState } from "react";
+import styled from "styled-components";
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { EventInfoContext, Guest } from "../_app";
+import GuestForm from "./guestForm";
 
-export default function AllGuests() {
 
 
+const TableWrapper = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Heading = styled.h3`
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const EachGuest = styled.div`
+  box-shadow: 5px 5px 5px gray;
+  border-radius: 5px;
+  margin: 8px;
+  padding: 6px;
+  background: #f7f4f2;
+  width: 23vw;
+`;
+
+const EachGuestInfo = styled.span`
+  padding: 2px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  color: black;
+`;
+
+export default function AllGuest() {
+  const router = useRouter();
+  const { guests, setGuests, expenses, setExpenses } = useContext(EventInfoContext);
+  const [sidePanel, setSidePanel] = useState(false);
+  const { slug, id } = router.query;
+  const [method, setMethod] = useState("POST");
+  const [tab, setTab] = useState("POST");
+  const [event_name, setEvent_name] = useState(
+    slug ? slug.toString() : "ERROR"
+  );
+  const [filteredGuests, setFilteredGuests] = useState(guests);
+  const [currentGuest, setCurrentGuest] = useState<Guest>({
+    firstName: "",
+    lastName: "",
+    gender: "MALE",
+    age: 0,
+    amountDue: 0,
+    RSVP: "PENDING",
+    notes: "",
+    event_id: Number(id),
+    event_name: event_name,
+  });
+  const removeGuest = async (id: number | undefined) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/guest/${id}`, {
+        method: "DELETE", // Use the appropriate HTTP method
+        headers: {
+          "Content-Type": "application/json",
+          // Add any additional headers if required
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const result = await response.json();
+      setGuests(result.guests);
+    } catch (error: any) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+  const edit = (guest: Guest) => {
+    setMethod("PUT");
+    setSidePanel(!sidePanel);
+    setCurrentGuest(guest);
+  };
+
+  const filterGuests = (criteria: string, guests: Guest[]) => {
+    const filteredList = guests.filter((guest) => {
+      return guest.event_name?.toLowerCase() === criteria;
+    });
+    setFilteredGuests(filteredList);
+  };
+console.log(expenses)
+  useEffect(() => {
+    if (typeof slug === "string") {
+      setEvent_name(slug);
+      filterGuests(slug, guests);
+    }
+  }, [guests, slug]);
+
+  const addGuest = () => {
+    setMethod("POST");
+    setSidePanel(!sidePanel);
+  };
   return (
     <div>
-ALL GUESTS
+      <Heading>
+        {event_name[0].toUpperCase() + event_name.slice(1)}'s Guest List
+      </Heading>
+      {sidePanel ? (
+        <GuestForm
+          setHidden={setSidePanel}
+          hidden={sidePanel}
+          currentGuest={currentGuest}
+          setCurrentGuest={setCurrentGuest}
+          method={method}
+        />
+      ) : (
+        <button onClick={() => addGuest()}> Add Guest</button>
+      )}
+      <TableWrapper>
+        {filteredGuests.length > 0
+          ? filteredGuests.map((guest, index) => (
+              <EachGuest key={guest.id}>
+                <button onClick={() => removeGuest(guest.id)}>-</button>
+                <button onClick={() => edit(guest)}>edit</button>
+                <span>#{index}</span>
+                <EachGuestInfo>
+                  <h3>
+                    {" "}
+                    {guest.firstName} {guest.lastName}
+                  </h3>
+                </EachGuestInfo>
+                <EachGuestInfo>{guest.gender}</EachGuestInfo>
+                <EachGuestInfo>Age: {guest.age} </EachGuestInfo>
+                <EachGuestInfo>RSVP: {guest.RSVP}</EachGuestInfo>
+                <EachGuestInfo>Amount due: ${guest.amountDue}</EachGuestInfo>
+                <EachGuestInfo>Notes: {guest.notes}</EachGuestInfo>
+                <EachGuestInfo>Event Name: {guest.event_name}</EachGuestInfo>
+              </EachGuest>
+            ))
+          : "no guest"}
+      </TableWrapper>
+
     </div>
   );
 }
