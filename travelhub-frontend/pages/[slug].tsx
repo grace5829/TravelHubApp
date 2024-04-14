6b9895c4493122b2805bf66e94e1156188b255f3
Grace Gao <grace5829@gmail.com>
2024-04-14 17:54:59 -0400

    passing title in caps to allguest and allexpenses

--- travelhub-frontend/pages/[slug].tsx
+++ travelhub-frontend/pages/[slug].tsx
@@ -1,14 +1,33 @@
 import Tabs from "./components/tabs";
 import AllGuests from "./components/allGuests";
 import AllExpenses from "./components/allExpenses";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function DynamicPage() {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const { slug, id } = router.query;
+
+
+  useEffect(() => {
+    if (typeof slug === "string") {
+     const words = slug
+        .split(" ")
+        ?.map((word) => {
+          return word[0].toUpperCase() + word.substring(1);
+        })
+        .join(" ");
+        setTitle(words);
+    }
+  }, [slug]);
   return (
     <div>
       <Tabs
         config={[
-          { header: "Guests", component: <AllGuests /> },
-          { header: "Expenses", component: <AllExpenses /> },
+          { header: "Guests", component: <AllGuests config={{title:`${title}`}}/> },
+          { header: "Expenses", component: <AllExpenses config={{title:`${title}`}}/> },
         ]}
       ></Tabs>
     </div>
