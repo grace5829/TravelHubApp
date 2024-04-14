be6109c885661af647ec2a02749ff259bf8151d2
Grace Gao <grace5829@gmail.com>
2024-04-14 12:31:50 -0400

    moving basic expenses to new tab

--- travelhub-frontend/pages/components/allExpenses.tsx
+++ travelhub-frontend/pages/components/allExpenses.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { EventInfoContext } from "../_app";
 
 export default function AllExpenses() {
+    const { expenses, setExpenses } = useContext(EventInfoContext);
 
 
   return (
     <div>
-All expenses
-    </div>
+      <div> 
+        {expenses? 
+     expenses.map((expense, index) => (
+      <div className={expense.name}>  {expense.name}</div>
+
+     ))
+     
+     : <div> No expenses</div>
+        
+        }
+      </div>    </div>
   );
 }
