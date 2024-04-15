75d66f296d2058eedf7e6faa93e8c1d4ae618a18
Grace Gao <grace5829@gmail.com>
2024-04-15 12:11:50 -0400

    new location for filtered guest

--- travelhub-frontend/pages/[slug].tsx
+++ travelhub-frontend/pages/[slug].tsx
@@ -1,16 +1,35 @@
 import Tabs from "./components/tabs";
 import AllGuests from "./components/allGuests";
 import AllExpenses from "./components/allExpenses";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { EventInfoContext } from "./_app";
 
 export default function DynamicPage() {
   const router = useRouter();
-
+  const { guests, setGuests } = useContext(EventInfoContext);
+  const [filteredGuests, setFilteredGuests] = useState([]);
   const [title, setTitle] = useState("");
   const { slug, id } = router.query;
 
 
+  const filterGuests = async (eventId:number) => {
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/guests/${eventId}`, {
+          method: "GET", // Use the appropriate HTTP method
+        });
+  
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+  
+        const result = await response.json();
+        setFilteredGuests(result.guests);
+      } catch (error: any) {
+        console.error("Error fetching data:", error.message);
+      }
+
+  };
   useEffect(() => {
     if (typeof slug === "string") {
      const words = slug
@@ -22,12 +41,17 @@ export default function DynamicPage() {
         setTitle(words);
     }
   }, [slug]);
+  useEffect(() => {
+    if (typeof slug === "string") {
+      filterGuests(Number(id));
+    }
+  }, [ guests]);
   return (
     <div>
       <Tabs
         config={[
-          { header: "Guests", component: <AllGuests config={{title:`${title}`}}/> },
-          { header: "Expenses", component: <AllExpenses config={{title:`${title}`}}/> },
+          { header: "Guests", component: <AllGuests config={{title:title, filteredGuests:filteredGuests}}/> },
+          { header: "Expenses", component: <AllExpenses config={{title:title, filteredGuests:filteredGuests}}/> },
         ]}
       ></Tabs>
     </div>
