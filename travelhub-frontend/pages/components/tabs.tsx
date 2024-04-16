8577a3efb3a07d81794f651d06498450a8f1fba6
Grace Gao <grace5829@gmail.com>
2024-04-16 11:51:10 -0400

    basic css stlying for tabs

--- travelhub-frontend/pages/components/tabs.tsx
+++ travelhub-frontend/pages/components/tabs.tsx
@@ -1,14 +1,42 @@
 import { useState } from "react";
-
+import styled from "styled-components";
+const EventInfoArea = styled.h3`
+  display: flex;
+  flex-direction: row;
+  // background:black
+`;
+const TabsArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: sticky;
+  top: 20px;
+  height:100%;
+  width:15vw;
+  font-size:10px;
+  z-index: 1;
+  top: 0; 
+  left: 0;
+  overflow-x: hidden; 
+  padding-top: 60px; 
+  `;
+const EachTab = styled.h3`
+  box-shadow: 5px 5px 5px gray;
+  border-radius: 5px;
+  margin: 8px;
+  padding: 6px;
+  background: #f7f4f2;
+`;
 export default function Tabs({ config }: any) {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div>
-      {config.map((entry: any, index: any) => (
-        <div onClick={() => setActiveTab(index)}>{entry.header}</div>
-      ))}
+    <EventInfoArea>
+      <TabsArea>
+        {config.map((entry: any, index: any) => (
+          <EachTab onClick={() => setActiveTab(index)}>{entry.header}</EachTab>
+        ))}
+      </TabsArea>
       <div>{config[activeTab].component}</div>
-    </div>
+    </EventInfoArea>
   );
 }
