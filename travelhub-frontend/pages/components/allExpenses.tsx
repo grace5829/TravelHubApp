bee9fc04c0d96be2601b39b31894cc275ca39cb1
Grace Gao <grace5829@gmail.com>
2024-04-22 13:43:41 -0400

    able to add new expense

--- travelhub-frontend/pages/components/allExpenses.tsx
+++ travelhub-frontend/pages/components/allExpenses.tsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect, useState } from "react";
-import { EventInfoContext } from "../_app";
+import { EventInfoContext, Expense } from "../_app";
 import styled from "styled-components";
 import { useRouter } from "next/router";
+import ExpenseForm from "./expenseForm";
 
 const Heading = styled.h3`
   margin: 10px;
@@ -55,33 +56,67 @@ float:right;
 `;
 export default function AllExpenses({ config }: any) {
   const { expenses, setExpenses } = useContext(EventInfoContext);
+  const router = useRouter();
 const [totalExpenses, setTotalExpenses]= useState(0)
+const [sidePanel, setSidePanel] = useState(false);
+const [method, setMethod] = useState("POST");
+const { slug, id } = router.query;
+  const [event_name, setEvent_name] = useState(
+    slug ? slug.toString() : "ERROR"
+  );
 const [totalExpensesPerPerson, setTotalExpensesPerPerson]= useState(0)
+const [currentExpense, setCurrentExpense] = useState<Expense>({
+  name: "",
+  description: "",
+  total: 0,
+  event_id: Number(id),
+  event_name: event_name,
+});
+
+
 useEffect(() => {
     let currentTotal=0
 expenses.map((expense)=>(
     currentTotal+=expense.total)
 )
-console.log(currentTotal/config.filteredGuests.length)
 setTotalExpensesPerPerson(currentTotal/config.filteredGuests.length.toFixed(2))
 setTotalExpenses(currentTotal)
 }, [expenses])
-
+const addExpense = () => {
+  setMethod("POST");
+  setSidePanel(!sidePanel);
+};
   return (
     <div>
 
       <Heading>{config.title}'s Expenses List</Heading>
+      {sidePanel ? (
+        <ExpenseForm
+          setHidden={setSidePanel}
+          hidden={sidePanel}
+          currentExpense={currentExpense}
+          setCurrentExpense={setCurrentExpense}
+          method={method}
+        />
+      ) : (
+        <button onClick={() => addExpense()}> Add Expense</button>
+      )}
     <ExpensesArea>
       {expenses ? (
         expenses.map((expense, index) => (
           <EachExpense>
+            
             <EachExpenseTitle className={expense.name + index}>
               {expense.name}
             </EachExpenseTitle>
             <EachExpenseBody>
+              {expense.description? 
+              
               <EachExpenseInfo className={expense.name + index}>
                 Description:{expense.description}
-              </EachExpenseInfo>
+              </EachExpenseInfo> :
+              null
+            }
               <EachExpenseInfo className={expense.name + index}>
                 ${expense.total}
               </EachExpenseInfo>
