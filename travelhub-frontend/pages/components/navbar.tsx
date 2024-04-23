537161d9f44224533fee486817bb1be63d5334ee
Grace Gao <grace5829@gmail.com>
2024-04-23 15:14:46 -0400

    adding icons to navBar

--- travelhub-frontend/pages/components/navbar.tsx
+++ travelhub-frontend/pages/components/navbar.tsx
@@ -10,17 +10,34 @@ export const IndividualLink = styled(Link)`
   font-size: 20px;
   padding-left: 5px;
   align-items-center;
+  display:flex;
 
   &:hover {
     background-color: #5fbbe3;
   }
 `;
+export const NavWrapper = styled.div`
+  color: black;
+  flex-wrap: wrap;
+
+  display: flex;
+`;
+export const IconWrapper = styled.div`
+  color: black;
+  align-items-center;
+
+`;
 
 export default function NavBar() {
   return (
-    <span>
-      <IndividualLink href="/">Home</IndividualLink>
-      <IndividualLink href="/events">Events</IndividualLink>
-    </span>
+    <NavWrapper>
+      <IndividualLink href="/">
+        <span className="material-symbols-outlined">home</span>Home
+      </IndividualLink>
+      <IndividualLink href="/events">
+        <span className="material-symbols-outlined">event</span>
+        Events
+      </IndividualLink>
+    </NavWrapper>
   );
 }
