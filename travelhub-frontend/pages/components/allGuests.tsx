f48d0ea6c6523ee818ceee76762f2e644a807d53
Grace Gao <grace5829@gmail.com>
2024-04-25 15:45:23 -0400

    fixing guest page css

--- travelhub-frontend/pages/components/allGuests.tsx
+++ travelhub-frontend/pages/components/allGuests.tsx
@@ -20,6 +20,11 @@ const Heading = styled.h3`
 
 const SubHeading = styled.div`
   margin: 10px;
+  float: right;
+width:100%;
+display: flex;
+align-items: center;
+justify-content:space-between;
 `;
 
 const EachGuest = styled.div`
@@ -34,13 +39,14 @@ const EachGuestInfo = styled.span`
   padding: 2px;
   display: flex;
   justify-content: center;
-  color:#f0e2d3;
+  color: #f0e2d3;
 `;
 const ButtonWrapper = styled.span`
   display: flex;
+  align-items: center;
   float: right;
   cursor: pointer;
-  font: 10px;
+  margin:2px;
 `;
 export default function AllGuest({ config }: any) {
   const router = useRouter();
@@ -101,27 +107,38 @@ export default function AllGuest({ config }: any) {
   return (
     <div>
       <Heading>{config.title} Guest List</Heading>
-      <SubHeading>Total guests:{guests.length} </SubHeading>
+      <SubHeading>Total guests:{guests.length} 
       {sidePanel ? (
-          <GuestForm
+        <GuestForm
           setHidden={setSidePanel}
           hidden={sidePanel}
           currentGuest={currentGuest}
           setCurrentGuest={setCurrentGuest}
           method={method}
-          />
-          ) : (
-              <button onClick={() => addGuest()}> Add Guest</button>
-              )}
+        />
+      ) : (
+          <ButtonWrapper onClick={() => addGuest()}>
+            <span className="material-symbols-outlined">person_add</span>
+            <span>New Guest</span>
+          </ButtonWrapper>
+    
+      )}
+      </SubHeading>
       <TableWrapper>
         {config.filteredGuests.map((guest: Guest, index: number) => (
           <EachGuest key={guest.id}>
-            <button onClick={() => removeGuest(guest.id)}><span className="material-symbols-outlined">
-delete
-</span></button>
-            <button onClick={() => edit(guest)}><span className="material-symbols-outlined">
-edit
-</span></button>
+            <ButtonWrapper
+              onClick={() => removeGuest(guest.id)}
+              className="material-symbols-outlined"
+            >
+              delete
+            </ButtonWrapper>
+            <ButtonWrapper
+              onClick={() => edit(guest)}
+              className="material-symbols-outlined"
+            >
+              edit
+            </ButtonWrapper>
             <EachGuestInfo>
               <h3>
                 {guest.firstName} {guest.lastName}
